15ea43d5cbd602a979d9d412c6f73d656aacb8fe
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-05-18 23:47:26 +0300

    Update urls.py

--- store/store/urls.py
+++ store/store/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.static import serve # Імпортуємо вбудований сервіс роздачі
 
 
